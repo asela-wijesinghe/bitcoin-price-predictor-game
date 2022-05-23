45b41448750e78d41c789ce032d7e3f62f2964aa
Asela Wijesinghe <aselawijesinghe@Aselas-MacBook-Pro.local>
2022-05-24 03:03:44 +0530

    fix default counter

--- FE/src/components/predict.component.js
+++ FE/src/components/predict.component.js
@@ -5,7 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { UPDATE_SCORE } from "../apis/score.api";
 
 const Predict = ({ score, user, setScoreChanged }) => {
-	const COUNTER_DEFAULT = 10;
+	const COUNTER_DEFAULT = 60;
 	const [counter, setCounter] = useState(COUNTER_DEFAULT);
 	const [currentPrice, setCurrentPrice] = useState(0);
 	const [guessResult, setGuessResult] = useState(null);
